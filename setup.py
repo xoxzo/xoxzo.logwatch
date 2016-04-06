4dd4b657b960c74130c6a76cadd319f5ad04badb
za <zaki@xoxzo.com>
2016-04-06 11:35:10 +0900

    Update version to 0.3 at setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xoxzo.logwatch",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         'Baker==1.3',
